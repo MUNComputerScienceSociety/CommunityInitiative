813850955a5441583851d0c690ac6443e94ae957
Whymarrh Whitby <whymarrh.whitby@mun.ca>
2013-10-07 00:24:44 +0000

    Initial commit.

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<link href="css/fresco/fresco.css" rel="stylesheet" type="text/css" />
+		<title>MUN CompSci Society Community Initiative</title>
+		<meta name="viewport" content="width=device-width" />
+		<meta name="description" content="The MUN CompSci Community Initiative is an effort by the MUN CompSci Society to help the community using Computer Science." />
+		<!-- Open Graph meta tags -->
+		<meta property="og:image" content="http://community.muncompsci.ca/images/og_autismsociety.png" />
+		<meta property="og:type" content="cause" />
+		<meta property="og:title" content="MUN CompSci Society Community Initiative" />
+		<meta property="og:site_name" content="MUN CompSci Society Community Initiative" />
+		<meta property="og:url" content="http://community.muncompsci.ca" />
+		<meta property="og:description" content="At noon on the 17th of Feb. 2013 NST the MUN CompSci Society will begin a 48hr Codeathon to create a webtool for the Autism Society of NL." />
+	</head>
+	<body class="clearfix">
+		<div id="left">
+			<h1>
+				<span class="muncs">MUN Comp Sci</span>
+				<span class="ci fs34">Community</span>
+				<span class="ci fs42">Initiative</span>
+			</h1>
+			<div class="line left"></div>
+			<div class="pres">Presents</div>
+			<div class="line right"></div>
+			<span class="hour">48 hour</span>
+			<span class="code">Codeathon</span>
+			<div class="divider"></div>
+			<span class="date">Feb 17 - Feb 19, 2013</span>
+			<div class="group">
+				<div class="clearfix">
+					<a href="http://twitter.com/muncssociety">
+						<img src="images/twitter.png" alt="Twitter Icon" />
+					</a>
+					<a href="http://www.facebook.com/groups/muncs/">
+						<img src="images/facebook.png" alt="Facebook Icon" />
+					</a>
+					<a class="button" href="https://www.canadahelps.org/services/wa/dnm/en/#/page/44">Donate</a>
+				</div>
+				<p>
+					All proceeds go to the Autism Society
+				</p>
+			</div>
+		</div>
+		<div id="content">
+			<h2>The codeathon has ended</h2>
+			<p>
+				The 48 hour Codeathon has officially ended.
+				You can follow what we&apos;ve done by checking out the tweets for <a href="https://twitter.com/search?q=CICodeathon">#CICodeathon</a>.
+				The code still needs a few touchups, but it's all open-source on <a href="https://github.com/MUNComputerScienceSociety/ABALookup">GitHub</a>.
+			</p>
+			<div class="divider"></div>
+			<h2>What will the code do?</h2>
+			<p>
+				The code developed during the 48 hour Codeathon will be a tool to facilitate the matching of parents of children with autism to potential <a href="https://en.wikipedia.org/wiki/Applied_behavior_analysis">ABA therapists</a>.
+				The application, called <em>ABA Lookup</em>, is the first of its kind, and will not only match parents and ABA therapists based on location, certification, and gender preference, but it will also take into consideration scheduling and availability.
+				The web app will include a graphical schedule component, which will list therapists&apos; availability.
+				For parents of children with autism, the process of finding ABA therapists can take weeks, if not months.
+				It is our hope - and end goal - that this tool will shorten the ABA and parent matching process exponentially.
+			</p>
+			<div class="divider"></div>
+			<h2>A big thanks to our sponsors</h2>
+			<p>
+				Along with many <a href="thanks/">student volunteers &amp; faculty</a>, we would like to thank the following sponsors:
+			</p>
+			<div id="sponsors">
+				<img src="images/autismsociety.png" alt="Autism Society of Newfoundland and Labrador" />
+				<br />
+				<img src="images/relish.png" alt="Relish Gourmet Burgers" />
+				<img src="images/staples.png" alt="Staples - Office Supplies, Technology, Ink and Much More" />
+				<br />
+				<img src="images/BostonPizza.jpg" alt ="Boston Pizza" />
+				<img src="images/facultyofscience.jpg" alt="Memorial University - Faculty of Science" />
+				<img src="images/treats.gif" alt="Treats" />
+				<br />
+				<img src="images/bennett.jpg" alt="Bennett Group of Companies" />
+			</div>
+			<div class="divider"></div>
+			<h2>What is a codeathon?</h2>
+			<p>
+				During the course of 48 hours, members of the Memorial University Computer Science Society designed, developed, and began testing a web-based application.
+				Our senior members lead the lesser experienced to produce a product that is both visually appealing and functional.
+				The MUN Computer Science Society&apos;s Community Initiative is a not-for-profit event, and all students involved are volunteering their time and skills for the selected cause.
+			</p>
+		</div>
+		<script src="js/jquery.js" type="text/javascript"></script>
+		<script src="js/fresco.js" type="text/javascript"></script>
+		<script async type="text/javascript">
+			WebFontConfig = {
+				google: { families: [ 'Open+Sans:700italic,400,800,300:latin' ] }
+			};
+			(function() {
+				var wf = document.createElement('script');
+				wf.src = 'http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+				wf.type = 'text/javascript';
+				wf.async = 'true';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(wf, s);
+			})();
+		</script>
+	</body>
+</html>
---
+++ thanks/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link href="../css/style.css" rel="stylesheet" type="text/css" />
+		<title>MUN CompSci Society Community Initiative</title>
+		<meta name="viewport" content="width=device-width" />
+		<meta name="description" content="The MUN CompSci Community Initiative is an effort by the MUN CompSci Society to help the community using Computer Science." />
+		<!-- Open Graph meta tags -->
+		<meta property="og:image" content="http://community.muncompsci.ca/images/og_autismsociety.png" />
+		<meta property="og:type" content="cause" />
+		<meta property="og:title" content="MUN CompSci Society Community Initiative" />
+		<meta property="og:site_name" content="MUN CompSci Society Community Initiative" />
+		<meta property="og:url" content="http://community.muncompsci.ca" />
+		<meta property="og:description" content="At noon on the 17th of Feb. 2013 NST the MUN CompSci Society will begin a 48hr Codeathon to create a webtool for the Autism Society of NL." />
+	</head>
+	<body class="clearfix">
+		<div id="left">
+			<h1>
+				<span class="muncs">MUN Comp Sci</span>
+				<span class="ci fs34">Community</span>
+				<span class="ci fs42">Initiative</span>
+			</h1>
+			<div class="line left"></div>
+			<div class="pres">Presents</div>
+			<div class="line right"></div>
+			<span class="hour">48 hour</span>
+			<span class="code">Codeathon</span>
+			<div class="divider"></div>
+			<span class="date">Feb 17 - Feb 19, 2013</span>
+			<div class="group">
+				<div class="clearfix">
+					<a href="http://twitter.com/muncssociety">
+						<img src="../images/twitter.png" alt="Twitter Icon" />
+					</a>
+					<a href="http://www.facebook.com/groups/muncs/">
+						<img src="../images/facebook.png" alt="Facebook Icon" />
+					</a>
+					<a class="button" href="https://www.canadahelps.org/services/wa/dnm/en/#/page/44">Donate</a>
+				</div>
+				<p>
+					All proceeds go to the Autism Society
+				</p>
+			</div>
+		</div>
+		<div id="content">
+			<h2>Project leads</h2>
+			<p>
+				<ul class="thanks">
+					<li>Lesley Chard - User experience</li>
+					<li>Mark Gauci - Project Manager</li>
+					<li>Ryan Martin - Quality control and documentation</li>
+					<li>Tim Oram - Development &amp; architecture</li>
+				</ul>
+			<p>
+			<div class="divider"></div>
+			<h2>The many great folks who helped out</h2>
+			<p>
+				<ul class="thanks cols">
+					<li>Chris Babstock</li>
+					<li>Richard Bajona</li>
+					<li>Andrew Caines</li>
+					<li>Noah Flemming</li>
+					<li>Gerry Hall</li>
+					<li>Lauren Mallay</li>
+					<li>Zack Mullaly</li>
+					<li>Adam Murphy</li>
+					<li>Ryan Murphy</li>
+					<li>Tim Murray</li>
+					<li>Chris Neville</li>
+					<li>Steve O'Keefe</li>
+					<li>Philip Okeke</li>
+					<li>Tyler Stacey</li>
+					<li>Michael Singleton</li>
+					<li>Mark Stacey</li>
+					<li>Whymarrh Whitby</li>
+					<li>Scott Young</li>
+				</ul>
+			</p>
+			<div class="divider"></div>
+			<h2>A special thanks to</h2>
+			<p>
+				<ul class="thanks">
+					<li>Dr. Andy Foster and the Faculty of Science for providing dinner and their overall support.</li>
+					<li>Dr. Todd Wareham for providing us with some much needed coffee.</li>
+					<li>Dr. Wolfgang Banzhaf and the Computer Science Department for all their support.</li>
+					<li>Ingrid Verbree-Barnes for providing us with some delicious cookies.</li>
+					<li>Joel Oram for providing a computer to stream the event.</li>
+					<li>Kelly Foss for getting the message out to the campus about the project.</li>
+					<li>Regina Edwards for providing support in all aspects of the project.</li>
+				</ul>
+			</p>
+		</div>
+		<script async type="text/javascript">
+			WebFontConfig = {
+				google: { families: [ 'Open+Sans:700italic,400,800,300:latin' ] }
+			};
+			(function() {
+				var wf = document.createElement('script');
+				wf.src = 'http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+				wf.type = 'text/javascript';
+				wf.async = 'true';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(wf, s);
+			})();
+		</script>
+	</body>
+</html>
